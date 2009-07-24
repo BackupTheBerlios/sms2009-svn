d4f3d4edf0923102fec1742ddbc5e1a814ab1766
magi <magi@7dedc00d-116d-0410-9c89-a45de4f404f2>
2009-07-24 13:58:38 +0000

    use doxygen for convection/diffusion example

--- trunk/cd_fd/cd.F90
+++ trunk/cd_fd/cd.F90
@@ -1,116 +1,116 @@
-! 1D Convection Diffusion equations solver in Fortran
-! Solves the equation:
-!
-! du   d       du           d u
-! -- = -- D(x) ---   + C(x) ---  + F(x) u - S(x)
-! dt   dx      dx           d x
-! 
-! for u, given functions for D, C, F, and S, defined in this program
-!
-! Explicit methods are used
-!
-! Jesse V. Johnson (jvj)
-! 6-9-09
+!> 1D Convection Diffusion equations solver in Fortran
+!!
+!! Solves the equation:
+!!
+!!\f[
+!!\frac{du}{dt}=\frac{d}{dx}D(x)\frac{du}{dx} + C(x)\frac{du}{dx}+F(x)u-S(x)
+!!\f]
+!! for \f$u\f$, given functions for \f$D\f$, \f$C\f$, \f$F\f$, and \f$S\f$, defined in this program
+!!
+!! Explicit methods are used
+!!
+!! \author Jesse V. Johnson (jvj)
+!! \date 6-9-09
 
-program convdiff
-    use derivatives
+program explicit_convdiff
+  use derivatives
+  implicit none
+
+  integer, parameter :: N=50 ! Number of nodes
+  real :: dt, t, tfinal ! Time domain
+  real :: xl, xr, dx   ! Space
+  real, dimension(:), allocatable :: x,u,dudx_store,convection,diffusion
+  real, dimension(:), allocatable :: d,c,s,f
+  integer i
+
+  allocate(u(N))
+  allocate(x(N))
+  allocate(d(N))
+  allocate(c(N))
+  allocate(s(N))
+  allocate(f(N))
+  allocate(dudx_store(N))
+  allocate(convection(N))
+  allocate(diffusion(N))
+
+  ! Set up grid 
+  ! Space
+  xl = 0.
+  xr = 1.
+  dx = real((xr - xl) / (N-1))
+  ! Time
+  t = 0.
+  tfinal = 0.05
+  dt = .0001
+
+  do i=1,N
+     x(i) = xl + real(dx * (i-1)) * (xr - xl)
+  enddo
+
+  ! Initial value
+  u = 0.
+
+  ! For now, start with a square wave.
+  u(N/2-3:N/2+3) = 1.
+
+  ! Thankfully, these are not time dependent.
+  d = dfun(x)
+  c = cfun(x)
+  f = ffun(x)
+  s = sfun(x)
+
+  time_loop: do while (t <= tfinal)
+
+     dudx_store = dudx(u,dx)
+
+     convection = c * dudx_store 
+     diffusion  = dudx(d,dx) * dudx_store + d * d2udx2(u,dx)
+
+     u = u + dt * (convection + diffusion + f * u + s)
+
+     t = t + dt
+     u(1)=0.
+     u(N)=0.
+
+     write (*,*) u
+  end do time_loop
+
+  deallocate(u)
+  deallocate(x)
+  deallocate(d)
+  deallocate(c)
+  deallocate(f)
+  deallocate(s)
+
+contains
+
+  elemental function dfun(x)
     implicit none
+    real, intent(in) :: x
+    real dfun
+    dfun = 0.
+  end function dfun
 
-    integer, parameter :: N=50 ! Number of nodes
-    real :: dt, t, tfinal ! Time domain
-    real :: xl, xr, dx   ! Space
-    real, dimension(:), allocatable :: x,u,dudx_store,convection,diffusion
-    real, dimension(:), allocatable :: d,c,s,f
-    integer i
+  elemental function cfun(x)
+    implicit none
+    real, intent(in) :: x
+    real cfun
+    cfun = 100.
+  end function cfun
 
-    allocate(u(N))
-    allocate(x(N))
-    allocate(d(N))
-    allocate(c(N))
-    allocate(s(N))
-    allocate(f(N))
-    allocate(dudx_store(N))
-    allocate(convection(N))
-    allocate(diffusion(N))
+  elemental function ffun(x)
+    implicit none
+    real, intent(in) :: x
+    real ffun
+    ffun = 0.
+  end function ffun
 
-    ! Set up grid 
-    ! Space
-    xl = 0.
-    xr = 1.
-    dx = real((xr - xl) / (N-1))
-    ! Time
-    t = 0.
-    tfinal = 0.05
-    dt = .0001
+  elemental function sfun(x)
+    implicit none
+    real, intent(in) :: x
+    real sfun
+    sfun = 0.
+  end function sfun
 
-    do i=1,N
-        x(i) = xl + real(dx * (i-1)) * (xr - xl)
-    enddo
-
-    ! Initial value
-    u = 0.
-
-    ! For now, start with a square wave.
-    u(N/2-3:N/2+3) = 1.
-
-    ! Thankfully, these are not time dependent.
-    d = dfun(x)
-    c = cfun(x)
-    f = ffun(x)
-    s = sfun(x)
-
-    time_loop: do while (t <= tfinal)
-        
-        dudx_store = dudx(u,dx)
-
-        convection = c * dudx_store 
-        diffusion  = dudx(d,dx) * dudx_store + d * d2udx2(u,dx)
-
-        u = u + dt * (convection + diffusion + f * u + s)
-
-        t = t + dt
-        u(1)=0.
-        u(N)=0.
-
-        write (*,*) u
-    end do time_loop
-
-    deallocate(u)
-    deallocate(x)
-    deallocate(d)
-    deallocate(c)
-    deallocate(f)
-    deallocate(s)
-
-    contains
-
-    elemental function dfun(x)
-     implicit none
-     real, intent(in) :: x
-     real dfun
-     dfun = 0.
-    end function dfun
-
-    elemental function cfun(x)
-     implicit none
-     real, intent(in) :: x
-     real cfun
-     cfun = 100.
-    end function cfun
-
-    elemental function ffun(x)
-     implicit none
-     real, intent(in) :: x
-     real ffun
-     ffun = 0.
-    end function ffun
-
-    elemental function sfun(x)
-     implicit none
-     real, intent(in) :: x
-     real sfun
-     sfun = 0.
-    end function sfun
-       
-end program convdiff
+end program explicit_convdiff
 
--- trunk/cd_fd/solvers.F90
+++ trunk/cd_fd/solvers.F90
@@ -1,36 +1,41 @@
+!> solve linear systems
+!!
+!! lifted from glimmer
 module solvers
-    contains
-      subroutine tridiag(a,b,c,x,y)
+contains
+  
+  !> solve a tridiagonal system
+  subroutine tridiag(a,b,c,x,y)
+    
+    !*FD Tridiagonal solver. All input/output arrays should have the 
+    !*FD same number of elements.
 
-        !*FD Tridiagonal solver. All input/output arrays should have the 
-        !*FD same number of elements.
+    real,dimension(:),intent(in) ::  a !< Lower diagonal; a(1) is ignored.
+    real,dimension(:),intent(in) ::  b !< Centre diagonal
+    real,dimension(:),intent(in) ::  c !< Upper diagonal; c(n) is ignored.
+    real,dimension(:),intent(out) :: x !< Unknown vector
+    real,dimension(:),intent(in) ::  y !< Right-hand side
+    
+    real,dimension(size(a)) :: aa
+    real,dimension(size(a)) :: bb
 
-        real,dimension(:),intent(in) ::  a !*FD Lower diagonal; a(1) is ignored.
-        real,dimension(:),intent(in) ::  b !*FD Centre diagonal
-        real,dimension(:),intent(in) ::  c !*FD Upper diagonal; c(n) is ignored.
-        real,dimension(:),intent(out) :: x !*FD Unknown vector
-        real,dimension(:),intent(in) ::  y !*FD Right-hand side
+    integer :: n,i
+    
+    n=size(a)
 
-        real,dimension(size(a)) :: aa
-        real,dimension(size(a)) :: bb
+    aa(1) = c(1)/b(1)
+    bb(1) = y(1)/b(1)
 
-        integer :: n,i
-
-        n=size(a)
-
-        aa(1) = c(1)/b(1)
-        bb(1) = y(1)/b(1)
-
-        do i=2,n
-           aa(i) = c(i)/(b(i)-a(i)*aa(i-1))
-           bb(i) = (y(i)-a(i)*bb(i-1))/(b(i)-a(i)*aa(i-1))
-        end do
+    do i=2,n
+       aa(i) = c(i)/(b(i)-a(i)*aa(i-1))
+       bb(i) = (y(i)-a(i)*bb(i-1))/(b(i)-a(i)*aa(i-1))
+    end do
         
-        x(n) = bb(n)
+    x(n) = bb(n)
 
-        do i=n-1,1,-1
-           x(i) = bb(i)-aa(i)*x(i+1)
-        end do
+    do i=n-1,1,-1
+       x(i) = bb(i)-aa(i)*x(i+1)
+    end do
 
   end subroutine tridiag
 
