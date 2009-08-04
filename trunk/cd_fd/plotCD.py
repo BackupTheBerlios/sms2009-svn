e338d6ab09c38fe1b8c5c0435be3d5426f65c57a
jessej <jessej@7dedc00d-116d-0410-9c89-a45de4f404f2>
2009-08-04 13:33:48 +0000

    Just added comments.

--- trunk/cd_fd/plotCD.py
+++ trunk/cd_fd/plotCD.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
 
+# Import only what is needed
 from numpy import loadtxt,shape,linspace
 from pylab import plot,show,clf,show,ion
 
-# Import
+# Import data file, called 'data'
 d=loadtxt('data')
+
+#Determine how much data came in
 dims = shape(d)
 
-clf()
-ion()
-x = linspace(0,1,dims[1])
-ph,=plot(x,d[0,:],'k')
-ph.figure.show()
+clf()    # Clears the screen
+ion()    # Interactive plot mode, critical for animation
+
+# x data, note that this must correspond to program's domain
+x = linspace(0,1,dims[1])  
+
+# Initial plot, very Matlab(ish), note return of plot handle that allows plot to
+# be altered elsewhere in code.
+ph,=plot(x,d[0,:],'k')     
+
+ph.figure.show()           # matplot lib requires show to be called
+
+# Loop to plot each time step
 for i in range(1,dims[0]):
-    ph.set_ydata(d[i,:])
+    ph.set_ydata(d[i,:])   # Only update y data (faster than replot)
     ph.figure.show()
