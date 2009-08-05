44fad8f6f869696a9ba43b866f19063dad4eb42d
icrutt <icrutt@7dedc00d-116d-0410-9c89-a45de4f404f2>
2009-08-05 18:42:18 +0000

    Hacked away at implicit solution

--- trunk/cd_fd/cd_implicit.F90
+++ trunk/cd_fd/cd_implicit.F90
@@ -13,52 +13,100 @@
 !! \date 6-27-09
 
 program implicit_convdiff
+
   use derivatives
   use solvers
 
   implicit none
-  integer, parameter :: N=100 ! Number of nodes
 
-  real :: dt, t, tfinal ! Time domain
-  real :: xl, xr, dx    ! Space domain
+  ! local variables
 
-  real, dimension(N) :: x,u,rhs,unew,uold
-  real, dimension(N) :: d,c,s,f,dddx,sub,dia,sup
+  integer, parameter :: N=100                   ! Number of nodes
+  real,    parameter :: dt = 0.005              ! length time step 
+  integer, parameter :: nt = 100                ! number of time steps
+  integer :: t                                  ! current time step
+  real :: xl                                    ! start of domain
+  real :: xr                                    ! end of domain 
+  real :: dx                                    ! node spacing
 
-  integer i
+  real, dimension(:), allocatable :: x          ! node positions
+  real, dimension(:), allocatable :: u          ! what we're solving for
+  real, dimension(:), allocatable :: rhs        ! Matrix equation right-hand side
+  real, dimension(:), allocatable :: unew       ! New values of u
+  real, dimension(:), allocatable :: uold       ! Old values of u
+  real, dimension(:), allocatable :: dddx       ! Spatial derivative of diffusivity
+  real, dimension(:), allocatable :: sub        ! Subdiagonal terms
+  real, dimension(:), allocatable :: dia        ! Diagonal terms
+  real, dimension(:), allocatable :: sup        ! Superdiagonal terms
+
+  real, dimension(:), allocatable :: d          ! diffusivity coeff
+  real, dimension(:), allocatable :: c          ! convection velocity
+  real, dimension(:), allocatable :: s          ! source term
+
+  integer :: ii                                 ! a counter
+  integer :: errstat                            ! for error checking
+  
+  ! let's allocate some memory
+  allocate(x(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate x")
+
+  allocate(u(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate u")
+
+  allocate(rhs(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate rhs")
+
+  allocate(unew(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate unew")
+
+  allocate(uold(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate uold")
+
+  allocate(dddx(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate dddx")
+
+  allocate(sub(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate sub")
+
+  allocate(dia(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate dia")
+
+  allocate(sup(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate sup")
+
+  allocate(d(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate d")
+
+  allocate(c(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate c")
+
+  allocate(s(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate s")
 
   ! Set up grid 
   ! Space
-  xl = 0.
-  xr = 1.
+  xl = 0.0
+  xr = 1.0
   dx = real((xr - xl) / (N-1))
 
-  do i=1,N
-     x(i) = xl + real(dx * (i-1)) * (xr - xl)
+  ! Initial value
+  do ii=1,N
+     x(ii)  =  xl + real(dx * (ii-1)) * (xr - xl)
   enddo
-
-  ! Time
-  t = 0.
-  tfinal = 0.5
-
+  
   ! Initial value
   ! For now, start with a square wave.
   uold = 0.
   uold(N/2-int(N*.05):N/2+int(N*.05)) = 1.
 
-  ! Thankfully, these are not time dependent, so we can simiply fill the
-  ! arrays
+  ! Set coefficients via function calls
+  ! Thankfully, these are not time dependent.
   d = dfun(x)
   c = cfun(x)
-  f = ffun(x)
   s = sfun(x)
 
   ! Need the derivative of d for calculations
   dddx = dudx(d,dx)
-
-  ! Compute the time step based on CFL condition
-  ! dt =min(dx**2./max(d),dx/max(c))
-  dt =.005
 
   ! Zero out the tridiagonal matrix
   sub = 0.
@@ -67,15 +115,15 @@ program implicit_convdiff
 
   ! Loop to place the coefficients into the matrix.
   ! Avoid boundaries this pass.
-  do i = 2,N-1
+  do ii = 2,N-1
      ! Left of point in question, or subdiagonal
-     sub(i) = dt / dx * (dddx(i) / 2. + d(i) / dx + c(i) / 2.)
+     sub(ii) = dt / dx * (dddx(ii) / 2. + d(ii) / dx + c(ii) / 2.)
      ! Point in question, or diagonal
-     dia(i) = dt  * (-2. * d(i) / dx**2. + f(i) - 1. / dt)
+     dia(ii) = dt  * (-2. * d(ii) / dx**2. - 1. / dt)
      ! Right of point in questions, superdiagonal
-     sup(i) = dt / dx * (dddx(i) / 2. + d(i) / dx - c(i) / 2.)
+     sup(ii) = dt / dx * (dddx(ii) / 2. + d(ii) / dx - c(ii) / 2.)
      ! Right hand side, or b in A x = b
-     rhs(i) = s(i) * dt 
+     rhs(ii) = s(ii) * dt 
   end do
 
   ! Set Dirchlet boundaries
@@ -84,12 +132,22 @@ program implicit_convdiff
   rhs(1) = 0.
   rhs(N) = 0.
 
-  time_loop: do while (t <= tfinal)
+  time_loop: do t =1,nt
      call tridiag(sub,dia,sup,unew,rhs - uold)
      write (*,*) unew
      uold = unew
-     t = t + dt
   end do time_loop
+
+  ! check that arrays are allocated before attempting to clean up
+  if(allocated(x))    deallocate(x)
+  if(allocated(u))    deallocate(u)
+  if(allocated(rhs))  deallocate(rhs)
+  if(allocated(unew)) deallocate(unew)
+  if(allocated(uold)) deallocate(uold)
+  if(allocated(dddx)) deallocate(dddx)
+  if(allocated(sub))  deallocate(sub)
+  if(allocated(dia))  deallocate(dia)
+  if(allocated(sup))  deallocate(sup)
 
 contains
 
@@ -104,15 +162,8 @@ contains
     implicit none
     real, intent(in) :: x
     real cfun
-    cfun = -1.
+    cfun = -1.0
   end function cfun
-
-  elemental function ffun(x)
-    implicit none
-    real, intent(in) :: x
-    real ffun
-    ffun = 0.
-  end function ffun
 
   elemental function sfun(x)
     implicit none
@@ -120,6 +171,16 @@ contains
     real sfun
     sfun = 0.
   end function sfun
+
+  subroutine checkerr(errstat,msg)
+    implicit none
+    integer,      intent(in) :: errstat
+    character(*), intent(in) :: msg 
+    if (errstat /= 0) then
+       write(*,*) "ERROR:", msg
+       stop
+    end if
+  end subroutine checkerr
 
 end program implicit_convdiff
 
