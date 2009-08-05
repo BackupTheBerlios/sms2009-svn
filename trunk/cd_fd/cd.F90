2dcb71d49363569b42a43a79732304aee0fce570
gethinwilliams <gethinwilliams@7dedc00d-116d-0410-9c89-a45de4f404f2>
2009-08-05 17:21:06 +0000

    re-jigged to include: more comments, error checking on dynamic mem, chnaged do while to plain do, removed friction coeff

--- trunk/cd_fd/cd.F90
+++ trunk/cd_fd/cd.F90
@@ -13,80 +13,109 @@
 !! \date 6-9-09
 
 program explicit_convdiff
+
   use derivatives
+
   implicit none
 
-  integer, parameter :: N=51 ! Number of nodes
-  real :: dt, t, tfinal ! Time domain
-  real :: xl, xr, dx   ! Space
-  real, dimension(:), allocatable :: x,u,dudx_store,convection,diffusion,ua
-  real, dimension(:), allocatable :: d,c,s,f
-  real :: pi
-  integer i
+  ! local variables
+  real, parameter    :: pi = 3.1415926536
 
-  pi = 3.1415926536
+  integer, parameter :: N=51                    ! Number of nodes
+  real,    parameter :: dt = 0.000125           ! length time step 
+  integer, parameter :: nt = 8208               ! number of time steps
+  integer :: t                                  ! current time step
+  real :: xl                                    ! start of domain
+  real :: xr                                    ! end of domain 
+  real :: dx                                    ! node spacing
 
-  allocate(u(N))
-  allocate(ua(N))
-  allocate(x(N))
-  allocate(d(N))
-  allocate(c(N))
-  allocate(s(N))
-  allocate(f(N))
-  allocate(dudx_store(N))
-  allocate(convection(N))
-  allocate(diffusion(N))
+  real, dimension(:), allocatable :: x          ! node positions
+  real, dimension(:), allocatable :: u          ! what we're solving for
+  real, dimension(:), allocatable :: dudx_store ! space to store du/dx
+  real, dimension(:), allocatable :: convection ! convection term
+  real, dimension(:), allocatable :: diffusion  ! diffusion term
+  real, dimension(:), allocatable :: ua         ! analytical sol'n
+
+  real, dimension(:), allocatable :: d          ! diffusivity coeff
+  real, dimension(:), allocatable :: c          ! convection velocity
+  real, dimension(:), allocatable :: s          ! source term
+
+  integer :: ii                                 ! a counter
+  integer :: errstat                            ! for error checking
+  
+  ! let's allocate some memory
+  allocate(u(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate u")
+
+  allocate(ua(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate ua")
+
+  allocate(x(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate x")
+
+  allocate(d(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate d")
+
+  allocate(c(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate c")
+
+  allocate(s(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate s")
+
+  allocate(dudx_store(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate dudx_store")
+ 
+  allocate(convection(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate convection")
+  
+  allocate(diffusion(N),stat=errstat)
+  call checkerr(errstat,"failed to allocate diffusion")
 
   ! Set up grid 
   ! Space
-  xl = 0.
-  xr = 1.
+  xl = 0.0
+  xr = 1.0
   dx = real((xr - xl) / (N-1))
-  ! Time
-  t = 0.
-!  tfinal = 1.025
-  tfinal = 1.026
-  dt = .000125
-!  dt = .006
 
   ! Initial value
-  do i=1,N
-     x(i)  =  xl + real(dx * (i-1)) * (xr - xl)
-     u(i)  =  sin(pi * x(i))
-     ua(i) = sin(pi * x(i))
+  do ii=1,N
+     x(ii)  =  xl + real(dx * (ii-1)) * (xr - xl)
+     u(ii)  =  sin(pi * x(ii))
+     ua(ii) =  sin(pi * x(ii))
   enddo
   
+  ! Set coefficients via function calls
   ! Thankfully, these are not time dependent.
   d = dfun(x)
   c = cfun(x)
-  f = ffun(x)
   s = sfun(x)
 
-  time_loop: do while (t <= tfinal)
+  time_loop: do t=1,nt 
 
      dudx_store = dudx(u,dx)
 
      convection = c * dudx_store 
      diffusion  = dudx(d,dx) * dudx_store + d * d2udx2(u,dx)
 
-     u = u + dt * (convection + diffusion + f * u + s)
+     u = u + dt * (convection + diffusion + s)
+     ua = sin(pi*x) * exp(-pi**2*(t*dt))
 
-     ua = sin(pi*x) * exp(-pi**2*t)
+     ! boundary conditions enforced
+     u(1)=0.0
+     u(N)=0.0
 
-     t = t + dt
-     u(1)=0.
-     u(N)=0.
-
+     ! Write out normalised error w.r.t. analytical sol'n
      write (*,*) 100.d0 * abs(u(2:N-1) - ua(2:N-1)) / ua(2-N-1)
+
   end do time_loop
 
-  deallocate(u)
-  deallocate(ua)
-  deallocate(x)
-  deallocate(d)
-  deallocate(c)
-  deallocate(f)
-  deallocate(s)
+  ! check that arrays are allocated before attempting to clean up
+  if(allocated(u))  deallocate(u)
+  if(allocated(ua)) deallocate(ua)
+  if(allocated(x))  deallocate(x)
+  if(allocated(d))  deallocate(d)
+  if(allocated(c))  deallocate(c)
+  if(allocated(s))  deallocate(s)
 
 contains
 
@@ -104,19 +133,22 @@ contains
     cfun = 0.
   end function cfun
 
-  elemental function ffun(x)
-    implicit none
-    real, intent(in) :: x
-    real ffun
-    ffun = 0.
-  end function ffun
-
   elemental function sfun(x)
     implicit none
     real, intent(in) :: x
     real sfun
     sfun = 0.
   end function sfun
+
+  subroutine checkerr(errstat,msg)
+    implicit none
+    integer,      intent(in) :: errstat
+    character(*), intent(in) :: msg 
+    if (errstat /= 0) then
+       write(*,*) "ERROR:", msg
+       stop
+    end if
+  end subroutine checkerr
 
 end program explicit_convdiff
 
